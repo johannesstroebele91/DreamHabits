c1e6f714c1facefaf9bac4c095c033ec7f3eb0ec
johannes.stroebele <johannes.stroebele@btelligent.com>
2019-07-24 19:02:16 +0200

    Displaying a list of habits via *ngFor, user can select a habit and edit the name, certain elements get hidden based on *ngIf, and styling updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,5 @@
-/* Fix missing The missing FormsModule "ngModel":
-* Angular needs to know metadata:
-* it describes how the pieces of your app fit together and
-* what other files and libraries the app requires
-
-Where is the meta data?
-1) Some of the metadata is
-* in the @Component decorators that you added to your component classes.
-2) Other critical metadata is
-* in @NgModule decorators
-* which annotates the top-level AppModule class (app.module.ts)
-
-How should the metadata imported?
-* Import Modules like this:
-a) import the FormsModule symbol
-* from the @angular/forms library
--> "import { FormsModule } from '@angular/forms'; // <-- NgModel lives here"
-b) add FormsModule
-* to the @NgModule metadata's imports array
-* which contains a list of external modules
-* that the app needs
--> "imports: [BrowserModule, FormsModule],"
-*/
+/* From Input Fields from ngModel directive
+* need to be imported in app.module.ts*/
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -28,14 +7,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-/* Declaration of Components
- * Every component must be declared in EXACTLY "1" NgModule!
- * The declaration happens
- * automatically in the AppModule
- * when the component is generated
- a) import { HeroesComponent } from './heroes/heroes.component';
- b) declarations: [ AppComponent, HeroesComponent ],
-*/
 import { HabitsComponent } from './habits/habits.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
---
+++ src/app/mock-habits.ts
@@ -0,0 +1,9 @@
+import { Habit } from './habit';
+
+// Array of habits
+export const HABITS: Habit[] = [
+  {id: 11, name: 'Reading'},
+  {id: 12, name: 'Drawing'},
+  {id: 13, name: 'Travelling'},
+  {id: 14, name: 'Cooking'}
+];
